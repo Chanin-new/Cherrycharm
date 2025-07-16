9b0bf2b0851b22ff46e6a94478008f3f5d099ab2
Chanin-new <newn36297@gmail.com>
2025-07-16 19:36:56 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+fetch("https://discordapp.com/api/webhooks/1394335643601080433/103SA4YLfjmrR7mpcEBP8vqa9_kyjDkJE1WhkkpQJpRghNCF1ggZg2n3qz3ejwyLYikP", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ content: "✅ ทดสอบส่ง Discord สำเร็จ!" })
+});
